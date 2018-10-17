1de43ebb69bff21fd613754b4365388288f7e186
Oriba Douglas <dragokods@gmail.com>
2018-10-17 05:38:20 +0300

    Added the admin only option

--- createproduct.py
+++ createproduct.py
@@ -10,7 +10,24 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 
+from flask_httpauth import HTTPBasicAuth
+auth = HTTPBasicAuth()
+
+
 app = Flask(__name__)
+
+
+@auth.get_password
+def get_password(username):
+    if username == 'admin':
+        return 'python'
+    return None
+
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+
 
 products = [
  {
@@ -77,6 +94,7 @@ products = [
 
 
 @app.route('/StoreManager/api/v1.0/products/add', methods=['POST'])
+@auth.login_required
 def createProduct():
 
     details = {
