4fec9ca5694d5309495833730c8b6c5ce39754ff
Oriba Douglas <dragokods@gmail.com>
2018-10-17 05:04:58 +0300

    One can get a product through the product id

---
+++ single_product.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 17 00:04:12 2018
+
+@author: XaXa
+"""
+
+
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+products = [
+ {
+
+  'id': 110,
+
+  'name': 'Rice',
+
+  'Price': '7,500 Ugx'
+
+ },
+
+ {
+
+  'id': 111,
+
+  'name': 'Mocassins',
+
+  'Price': '75,000 Ugx'
+
+ },
+
+ {
+
+  'id': 112,
+
+  'name': 'T-Shirt',
+
+  'Price': '10,000 Ugx'
+
+ },
+
+ {
+
+  'id': 113,
+
+  'name': 'Timbaland',
+
+  'Price': '150,000 Ugx'
+
+ },
+
+ {
+
+  'id': 114,
+
+  'name': 'Maize Flour',
+
+  'Price': '80,000 Ugx'
+
+ },
+
+ {
+
+  'id': 115,
+
+  'name': 'Jean Trousers',
+
+  'Price': '30,000 Ugx'
+
+ }
+
+ ]
+
+
+@app.route('/StoreManager/api/v1.0/products/<productId>', methods=['GET'])
+def getProduct(productId):
+    serial = [prod for prod in products if (prod['id'] == productId)]
+    return jsonify({'prod': serial})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
