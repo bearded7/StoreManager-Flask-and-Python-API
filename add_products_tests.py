39fa7fca06193aec4ac9fcd2066a279ef345cf80
Oriba Douglas <dragokods@gmail.com>
2018-10-19 09:32:36 +0300

    added unit tests

---
+++ add_products_tests.py
@@ -0,0 +1,19 @@
+import unittest
+import json
+import requests 
+from createproduct import products
+from unittest import TestCase
+
+def test_admin_can_add_product(self):
+    res = self.admin.post(
+        'StoreManager/api/v1/AddProducts'
+    )
+    self.assertEqual(200, res.status_code)
+
+def test_route_admin_can_add_product_requires_login(self):
+    res = self.admin.get(
+        'StoreManager/api/v1/Products', follow_redirects=True)
+    self.assertIn(b'Please log in to access this page', response.data)
+
+if __name__ == '__main__':
+    unittest.main()
