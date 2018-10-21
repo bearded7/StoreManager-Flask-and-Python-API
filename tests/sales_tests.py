aa6aa27b3e229d8c3fcaccd51e53a8d39be8a2b7
bearded7 <dragokods@gmail.com>
2018-10-21 21:13:27 +0300

    Update tests/sales_tests.py
    import module

--- tests/sales_tests.py
+++ tests/sales_tests.py
@@ -2,16 +2,17 @@ import unittest
 import json
 import requests 
 from unittest import TestCase
+from endpoints.sales import app
 
 def test_admin_can_get_all_sale_records(self):
-    res = self.admin.get(
-        'StoreManager/api/v1/GetAllSales'
+    response = self.admin.get(
+        'StoreManager/api/v1/GetAllSales/'
     )
-    self.assertEqual(200, res.status_code)
+    self.assertEqual(200, response.status_code)
 
 def test_route_get_all_sales_requires_login(self):
-    res = self.admin.get(
-        'StoreManager/api/v1/GetAllSales', follow_redirects=True)
+    response = self.admin.get(
+        'StoreManager/api/v1/GetAllSales/', follow_redirects=True)
     self.assertIn(b'Please log in to access this page', response.data)
 
 if __name__ == '__main__':
