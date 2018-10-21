5459bcf601d221874d177c5516c63eba3d62446b
bearded7 <dragokods@gmail.com>
2018-10-21 21:11:53 +0300

    Update tests/add_products_tests.py
    import tests

--- tests/add_products_tests.py
+++ tests/add_products_tests.py
@@ -2,16 +2,17 @@ import unittest
 import json
 import requests 
 from unittest import TestCase
+from endpoints.createproduct import app 
 
 def test_admin_can_add_product(self):
-    res = self.admin.post(
-        'StoreManager/api/v1/AddProducts'
+    response = self.admin.post(
+        'StoreManager/api/v1/AddProducts/'
     )
-    self.assertEqual(200, res.status_code)
+    self.assertEqual(200, response.status_code)
 
 def test_route_admin_can_add_product_requires_login(self):
-    res = self.admin.get(
-        'StoreManager/api/v1/Products', follow_redirects=True)
+    response = self.admin.get(
+        'StoreManager/api/v1/Products/', follow_redirects=True)
     self.assertIn(b'Please log in to access this page', response.data)
 
 if __name__ == '__main__':
